55cdcfdb29e36581d9b071ab76b53d9db64ce6ec
Marcio Almeida <marcioalmeida13495@gmail.com>
2024-09-22 18:57:51 -0300

    Third commit

--- src/Components/Post/index.jsx
+++ src/Components/Post/index.jsx
@@ -2,7 +2,7 @@ export const Post = ({post}) => {
     
     return (
         <div className='post'>
-            <img src={post.cover} />
+            <img src={post.cover} alt={post.title} />
             <div className='post-content'>
                 <h1>{post.title}</h1>
                 <p>{post.body}</p>
