da0f0a7ebf605f32de93b9454d3c74bcf8545577
Marcio Almeida <marcioalmeida13495@gmail.com>
2024-09-23 14:41:04 -0300

    v2309241441

---
+++ src/Templates/Home.jsx
@@ -0,0 +1,49 @@
+import './styles.css'
+import { Component } from 'react';
+import { loadPosts } from '../Utils/loadPosts';
+import { Posts } from '../Components/Posts';
+//import { StaticImg } from '../Components/StaticImg';
+
+export class Home extends Component{
+  
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPages: 8
+  };
+  componentDidMount(){
+    this.loadPosts();
+  }
+ 
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    const {page, postsPerPages} = this.state;
+    this.setState({allPosts: postsAndPhotos,posts: postsAndPhotos.slice(page*postsPerPages, ((page+1)*postsPerPages)),
+        page: page+1
+    });
+    console.log(postsAndPhotos.slice(page*postsPerPages , ((page+1)*postsPerPages)))
+  }
+
+  componentDidUpdate(){
+  }
+  
+  handleNextPage = () =>{ 
+    const {page, allPosts, postsPerPages, posts} = this.state;
+    
+    this.setState({posts: posts.concat(allPosts.slice(page*postsPerPages , ((page+1)*postsPerPages))),
+      page: page+1
+  });
+  }
+
+  render() {
+    const { posts } = this.state;
+    return(
+      <section className='container'>
+         
+         <Posts posts={ posts }></Posts>
+         <button onClick={this.handleNextPage} >LOAD MORE POSTS</button>
+      </section>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/global-styles.css';
 import reportWebVitals from './reportWebVitals';
+import { Home } from './Templates/Home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    <App />
+    <Home />
   </>
 );
 
